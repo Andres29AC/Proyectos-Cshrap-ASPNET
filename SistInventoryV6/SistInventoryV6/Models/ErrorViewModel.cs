31dcd1cd6e330f30c984aebb8f72fb4448ac5a4d
Andres Amaro <lionAS_45@hotmail.com>
2023-10-15 01:15:28 -0500

    Commit one project

---
+++ SistInventoryV6/SistInventoryV6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistInventoryV6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

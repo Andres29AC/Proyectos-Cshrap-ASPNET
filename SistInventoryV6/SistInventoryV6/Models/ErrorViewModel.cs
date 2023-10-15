6a75bbad3dbc714a9ff0b9c335643c88d8ed2579
Andres Amaro <lionAS_45@hotmail.com>
2023-10-15 02:57:22 -0500

    Eliminar directorio SistInventoryV6

--- SistInventoryV6/SistInventoryV6/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SistInventoryV6.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
